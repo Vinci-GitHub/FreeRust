9f6538669bad8016dcac9f2b3274932a1d5996f6
Vinci-GitHub <learning.never.exhausts.the.mind9@gmail.com>
2021-01-10 19:25:21 +0900

    guessgame

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+/Users/zen/Desktop/FreeRust/guessing_game/target/debug/deps/guessing_game: src/main.rs
+
+/Users/zen/Desktop/FreeRust/guessing_game/target/debug/deps/guessing_game.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/zen/Desktop/FreeRust/guessing_game/target/debug/guessing_game: /Users/zen/Desktop/FreeRust/guessing_game/src/main.rs
