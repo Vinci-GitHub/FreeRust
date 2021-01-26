f5ef01f9e2d26b5ef5e13514c08f533358e4fd0a
Vinci-GitHub <learning.never.exhausts.the.mind9@gmail.com>
2021-01-27 07:29:40 +0900

    変数宣言

--- new1/src/main.rs
+++ new1/src/main.rs
@@ -1,3 +1,5 @@
+const PI: f32 = 3.14159;
+
 //関数の引数は`(変数名: 型名、 )`で書く
 fn fizzbuzz(n: usize) {
     for i in 0..n {
@@ -15,4 +17,29 @@ fn fizzbuzz(n: usize) {
 
 fn main() {
     fizzbuzz(20);
+
+    // xの型を推論
+    let x = 13;
+    println!("{}", x);
+
+    // xの型を指定
+    let x: f64 = 3.14159;
+    println!("{}", x);
+
+    // 宣言の後で初期化
+    let x;
+    x = 0;
+    println!("{}", x);
+
+    let mut x = 42;
+    println!("{}", x);
+    x = 13;
+    println!("{}", x);
+
+    let a = 13u8;
+    let b = 7u32;
+    let c = a as u32 + b;
+    println!("{}", c);
+
+    println!("ゼロからアップル{}を作るには", PI);
 }
