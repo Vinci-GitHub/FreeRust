b0ff639c38cb23441a4c464bee76664bbd3e47ed
Vinci-GitHub <learning.never.exhausts.the.mind9@gmail.com>
2021-01-30 06:10:32 +0900

    構造体

--- new1/src/main.rs
+++ new1/src/main.rs
@@ -13,9 +13,31 @@ fn main() {
     // インスタンスを使ってメソッドを呼び出す。
     println!("{} is {}", s, s.len());
 
+    // SeaCreatureのデータはスタックに入ります
     let ferris = SeaCreature {
+        // String構造体もスタックに入りますが、
+        // ヒープに入るデータの参照アドレスが一つ入ります。
         animal_type: String::from("crab"),
+        name: String::from("Ferris"),
+        arms: 2,
+        legs: 4,
+        wepon: String::from("claw"),
     };
 
-    println!("{}", ferris);
+    let sarah = SeaCreature {
+        animal_type: String::from("octopus"),
+        name: String::from("Sarah"),
+        arms: 8,
+        legs: 0,
+        wepon: String::from("none"),
+    };
+
+    println!(
+        "{} is a {}. They have {} arms, {} legs, and a {} wepon",
+        ferris.name, ferris.animal_type, ferris.arms, ferris.legs, ferris.wepon
+    );
+    println!(
+        "{} is a {}. They have {} arms . and {} legs. They have no wepon..",
+        sarah.name, sarah.animal_type, sarah.arms, sarah.legs
+    );
 }
