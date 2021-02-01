f047c0f9c2dfa0bed2729640e63e861148b2554f
Vinci-GitHub <learning.never.exhausts.the.mind9@gmail.com>
2021-02-02 05:45:46 +0900

    option

--- new1/src/main.rs
+++ new1/src/main.rs
@@ -1,9 +1,32 @@
-enum Item {
-    Inventory(String),
-    // Noneには項目が無いことを表す
-    None,
+// 部分的に定義された構造体
+struct BagOfHolding<T> {
+    // パラメータTを渡す事が可能
+    item: Option<T>,
 }
 
-struct BagOfHolding {
-    item: Item,
+fn main() {
+    // i32が入るバッグには何も入っていません
+    // Noneからは型が決められない為型をしているする必要があります
+    let i32_bag = BagOfHolding::<i32> { item: None };
+
+    if i32_bag.item.is_none() {
+        println!("バッグには何もない")
+    } else {
+        println!("バッグにはなにかある")
+    }
+
+    let i32_bag = BagOfHolding::<i32> { item: Some(42) };
+
+    if i32_bag.item.is_some() {
+        println!("バッグにはなにかある")
+    } else {
+        println!("バッグには何もない!")
+    }
+
+    // match はOptionをエレガントに分解して
+    // すべてのケースが処理されることを保証できます
+    match i32_bag.item {
+        Some(v) => println!("バッグに {} を発見", v),
+        None => println!("何も見つからなかった"),
+    }
 }
